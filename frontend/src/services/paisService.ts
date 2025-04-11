ed1dc9da5028ff1f16c00404451e468df92ccbc5
pedro garcia <pedrofsgarcia.pro@gmail.com>
2025-04-11 08:26:54 -0300

    feat(paisService): conecta com o back

---
+++ frontend/src/services/paisService.ts
@@ -0,0 +1,131 @@
+export async function buscaPaisPorNome(nome:string){
+    try {
+        const baseUrl = import.meta.env.VITE_BACKEND_URL;
+        
+        const url = new URL(`${baseUrl}/busca_pais_por_nome`);
+        url.searchParams.append('nome', nome);
+    
+        const response = await fetch (url.toString(),{
+            method:"GET",
+            headers: {
+                "Accept" : "application/json"
+            } 
+        });
+    
+        const data = await response.json();
+    
+        if (response.status == 200) {
+            console.log(data.resposta);
+            return data.resposta
+        } else {
+            throw new Error (data.error || "Erro desconhecido")
+        }
+    } catch (error) {
+        console.error("Erro ao acessar servidor:", error);
+        alert(error instanceof Error ? error.message : 'Erro desconhecido');
+        throw error;
+    }
+}
+
+
+export async function buscarRankingPaises(
+    tipo: string,
+    ncm: number[],
+    qtd: number,
+    paises?: number[],
+    estados?: number[],
+    anos?: number[],
+    meses?: number[],
+    crit?:  "valor_fob"|"kg_liquido"|"valor_agregado"|"registros",
+    cresc: 0 | 1 = 0
+){
+    try {
+        const base_url = import.meta.env.VITE_BACKEND_URL;
+        const url = new URL(`${base_url}/ranking_paises`);
+        url.searchParams.append('tipo', tipo);
+        url.searchParams.append('qtd', qtd.toString());
+        if (crit) {
+            url.searchParams.append('crit', crit );
+        }
+        
+        url.searchParams.append('cresc', cresc.toString())
+        
+        const appendListParams = (paramName: string, values?: number[]) => {
+            values?.forEach(value => url.searchParams.append(paramName, value.toString()));
+        };
+        appendListParams('ncm', ncm);
+        appendListParams('paises', paises);
+        appendListParams('estados', estados);
+        appendListParams('anos', anos);
+        appendListParams('meses', meses);
+
+        const response = await fetch (url.toString(),{
+            method:"GET",
+            headers: {
+                "Accept" : "application/json"
+            } 
+        });
+
+        const data = await response.json();
+
+        if (response.status == 200) {
+            console.log(data.resposta);
+            return data.resposta
+        } else{
+            throw new Error (data.error || "Erro desconhecido")
+        }
+
+    } catch (error) {
+        console.error("Erro ao acessar servidor:", error);
+        alert(error instanceof Error ? error.message : 'Erro desconhecido');
+        throw error;
+    }
+}
+
+export async function buscarHistoricoPais(
+    tipo: string,
+    paises: number[],
+    ncm?: number[],
+    estados?: number[],
+    anos?: number[],
+    meses?: number[],
+    via?: number[],
+    urfs?: number[]
+){
+    try {
+        const base_url = import.meta.env.VITE_BACKEND_URL;
+        const url = new URL(`${base_url}/busca_pais_hist`);
+        url.searchParams.append('tipo', tipo);
+
+        const appendListParams = (paramName: string, values?: number[]) => {
+            values?.forEach(value => url.searchParams.append(paramName, value.toString()));
+        };
+        appendListParams('paises', paises);
+        appendListParams('ncm', ncm);
+        appendListParams('estados', estados);
+        appendListParams('anos', anos);
+        appendListParams('meses', meses);
+        appendListParams('via', via);
+        appendListParams('urfs', urfs);
+
+        const response = await fetch (url.toString(),{
+            method:"GET",
+            headers: {
+                "Accept" : "application/json"
+            } 
+        });
+
+        const data = await response.json();
+
+        if (response.status == 200) {
+            console.log(data.resposta);
+            return data.resposta
+        } else{
+            throw new Error (data.error || "Erro desconhecido")
+        }
+    } catch (error) {
+        console.error("Erro ao acessar servidor:", error);
+        alert(error instanceof Error ? error.message : 'Erro desconhecido');
+        throw error;
+    }
+}
